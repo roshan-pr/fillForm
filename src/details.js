6a6856b418843e464e17c0b6aaffcbd517e963a2
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-06-06 15:35:21 +0530

    Takes info to the Details as in an array

--- src/details.js
+++ src/details.js
@@ -1,5 +1,42 @@
-const details = function() {
+class Details {
+  constructor() {
+    this.info = [];
+  }
 
+  addInfo(detail) {
+    this.info.push(detail);
+  }
+
+  print() {
+    console.log(this.info);
+  }
+}
+
+const readLines = (personDetails) => {
+  let messages = ['Name :', 'Dob :', 'Hobbies :'];
+  let messageIndex = 0;
+  console.log(messages[messageIndex]);
+
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('data', (detail) => {
+    if (messageIndex >= messages.length) {
+      personDetails.print();
+      process.exit(0);
+    }
+    messageIndex++;
+    console.log(messages[messageIndex]);
+    personDetails.addInfo(detail);
+  });
+
+  process.stdin.on('end', () => {
+
+  })
 };
 
-exports.details = details;
+const details = function () {
+
+  const personDetails = new Details();
+  readLines(personDetails);
+};
+
+details();
