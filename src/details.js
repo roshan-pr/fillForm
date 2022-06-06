1cf7147ef4af197d59596b8f092e6446cea4b194
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-06-06 17:02:55 +0530

    Implemented addDOB
    * Will works for given queries

--- src/details.js
+++ src/details.js
@@ -2,8 +2,10 @@ process.stdin.setEncoding('utf8');
 
 class PersonalDetails {
   #name;
+  #dob;
   constructor() {
     this.#name = '';
+    this.#dob = '';
   }
 
   addName(name) {
@@ -11,34 +13,58 @@ class PersonalDetails {
     return true;
   }
 
+  addDOB(dob) {
+    this.#dob = dob;
+    return true;
+  }
+
   toString() {
-    return `${this.#name}`
+    return `Name: ${this.#name} DOB: ${this.#dob}`
   }
 }
+
+const readLine = (messages) => {
+  let messageIndex = 0;
+  return (chunk) => {
+    const detail = chunk.split('\n')[0];
+    if (messages[messageIndex].action(detail)) {
+      messageIndex++;
+      if (messageIndex >= messages.length) {
+        process.stdin.emit('end');
+        process.exit(0);
+      }
+      console.log(messages[messageIndex].msg);
+    } else {
+      console.log('Wrong input');
+    }
+  }
+};
 
 const readLines = (personDetails, messages) => {
   let messageIndex = 0;
   console.log(messages[messageIndex].msg);
 
-  process.stdin.on('data', (detail) => {
-    if (messages[messageIndex].action(detail)) {
-      console.log('Added ', personDetails + '');
-      messageIndex++;
-    } else {
-      console.log('Wrong input');
-    }
-  });
+  process.stdin.on('data', readLine(messages));
 
   process.stdin.on('end', () => {
-    console.log(Thanks);
+    console.log('\nThanks');
+    console.log(personDetails + '');
   })
 };
 
 const details = function () {
   const personDetails = new PersonalDetails();
 
-  let messages = [
-    { msg: 'Please enter your name :', action: (name) => personDetails.addName(name) }];
+  const messages = [
+    {
+      msg: 'Please enter your name :',
+      action: (name) => personDetails.addName(name)
+    },
+    {
+      msg: 'Please enter your DOB :',
+      action: (dob) => personDetails.addDOB(dob)
+    }
+  ];
   readLines(personDetails, messages);
 };
 
